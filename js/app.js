3e86b20f79d169fa36fd3c9f39c2fb50d56fd4ff
Samuel Murray <smurray24@gmail.com>
2018-05-28 13:19:32 -0700

    Create the individual cards

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
 /*
  * Create a list that holds all of your cards
  */
-const suits = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor","fa fa-bolt", "fa fa-cube", "fa fa-anchor", "fa fa-leaf", "fa fa-bicycle", "fa fa-diamond","fa fa-bomb", "fa fa-leaf", "fa fa-bomb", "fa fa-bolt", "fa fa-bicycle", "fa fa-paper-plane-o", "fa fa-cube"];
+const suits = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor","fa fa-bolt", "fa fa-cube", "fa fa-anchor", 
+"fa fa-leaf", "fa fa-bicycle", "fa fa-diamond","fa fa-bomb", "fa fa-leaf", "fa fa-bomb", "fa fa-bolt", "fa fa-bicycle", 
+"fa fa-paper-plane-o", "fa fa-cube"];
+
+/* Create the individual cards */
+const cardContainer = document.querySelector(".deck");
+
+for(let i = 0; i < suits.length; i++) {
+	const card = document.createElement("li");
+	card.classList.add ("card");
+	card.innerHTML = "<i class'" + suits[i] + "'</i>";
+	cardContainer.appendChild(card);
+}
 
 /*
  * Display the cards on the page
