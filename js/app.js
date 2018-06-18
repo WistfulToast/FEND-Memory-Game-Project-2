e0eb283fdad7bd6b57cf065e0b29cdfea0c6f7ad
Samuel Murray <smurray24@gmail.com>
2018-06-17 17:36:07 -0700

    Corrected bug allowing for multiple clicks

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 /*
  * Create a list that holds all of your cards
  */
-const suits = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor","fa fa-bolt", "fa fa-cube", "fa fa-anchor", 
-"fa fa-leaf", "fa fa-bicycle", "fa fa-diamond","fa fa-bomb", "fa fa-leaf", "fa fa-bomb", "fa fa-bolt", "fa fa-bicycle", 
-"fa fa-paper-plane-o", "fa fa-cube"];
+const suits = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor","fa fa-bolt", "fa fa-cube", "fa fa-anchor",
+"fa fa-leaf", "fa fa-bicycle", "fa fa-diamond","fa fa-camera-retro", "fa fa-leaf", "fa fa-camera-retro", "fa fa-bolt", "fa fa-bicycle", 
+"fa fa-paper-plane-o", "fa fa-cube"];	
 
 /* Create the individual cards */
 const cardContainer = document.querySelector(".deck");
@@ -11,8 +11,35 @@ const cardContainer = document.querySelector(".deck");
 let flippedCards = [];
 let matchedCards = [];
 
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
+
+
 function start() {
 for(let i = 0; i < suits.length; i++) {
+	const layout = shuffle(suits);
 	const card = document.createElement("li");
 	card.classList.add("card");
 	card.innerHTML = `<i class ="${suits[i]}"> </i>`;
@@ -60,19 +87,65 @@ function check (selectedCard, priorCard) {
 
 		selectedCard.classList.remove("open", "show", "selected");
 		priorCard.classList.remove("open", "show", "selected");
-		flippedCards = [];
 	}, 750);
+		flippedCards = [];
 
 	}
+/*Making a Move*/
+	addMove();
+	addScore();
+
 }
 
 function isOver() {
 	if(matchedCards.length === suits.length) {
-		alert("Game Over.  You Win!");
+		alert("Game Over.  You Win!  Would you like to play again?");
+	}
 }
 
+/*Adding Move to the Counter*/
+const numOfMovesContainer = document.querySelector(".moves");
+let numOfMoves = 0;
+function addMove(){
+	numOfMoves++;
+	numOfMovesContainer.innerHTML = numOfMoves;
 }
- /*Start Game */
+
+/* Ratings */
+
+
+const starsContainer = document.querySelector(".stars");
+const star = '<li><i class="fa fa-star"></i></li>';
+starsContainer.innerHTML = star + star + star;
+function score() {
+
+    if(numOfMoves < 2) {
+        starsContainer.innerHTML = star + star + star;
+    } else if(numOfMoves < 5) {
+        starsContainer.innerHTML = star + star;
+    } else {
+        starsContainer.innerHTML = star;
+    }
+}
+
+
+/* Restart Game */
+const gameRestart = document.querySelector(".restart");
+gameRestart.addEventListener("click", function()
+{
+/*Delete Cards in List */
+	cardContainer.innerHTML = "";
+/*Call 'Start' */
+	start();
+	matchedCards = [];
+	numOfMoves = 0;
+	numOfMovesContainer.innerHTML = numOfMoves;
+	starsContainer.innerHTML = star + star + stars;
+
+/*Start Game */
+
+ /*Reset Variables */
+});
 start();
 
 
